2fa671e7c97d2e72f61fd641eacedce61c29e31c
H26 <josephh2601@gmail.com>
2023-10-28 23:15:20 +0100

    Improved Physics
    Added acceleration and speed for more realistic movement and linked rudder movement to speed - which inverts when going backwards.

--- Code/main.py
+++ Code/main.py
@@ -7,14 +7,23 @@ import math
 # Setup
 pygame.init()
 
-#Interpulation graph
-x = np.array([-360,0,90,180,270,360])
-y = np.array([-1,-0.1,1,0.4,1,-0.1])
-speedfunction = CubicHermiteSpline(x=x, y=y, dydx=np.zeros_like(y))
+#accelerationFunction graph
+x = np.array([-180,0,100,180])
+y = np.array([-0.4,-0.007,0.5,0.2])
+accelerationFunction = CubicHermiteSpline(x=x, y=y, dydx=np.zeros_like(y))
+
+#maxSpeed graph
+x = np.array([-180,0,30,100,180])
+y = np.array([0.5,0.7,0.8,1.1,0.4])
+# y = np.array([1, 1, 1, 1, 1])
+maxSpeed = CubicHermiteSpline(x=x, y=y, dydx=np.zeros_like(y))
+
+#minSpeed
+minSpeed = -0.8
 
 # Window initiation
-screenWidth = 800
-screenHeight = 600
+screenWidth = 900
+screenHeight = 700
 screen = pygame.display.set_mode((screenWidth, screenHeight))
 
 pygame.display.set_caption("SailingSim")
@@ -22,18 +31,21 @@ icon = pygame.image.load("../Assets/Boat.png")  # To change Icon
 pygame.display.set_icon(icon)
 
 def localWind(posx, posy):
-    return(2,0)
+    return(5,0)
 
 
 class Boat(pygame.sprite.Sprite):
 
     def __init__(self):
         super().__init__()
-        self.scale = 0.05
+        self.scale = 0.1
         self.posx = 400
         self.posy = 300
         self.angle = 0
-        self.angular_velocity = 0
+        self.speed = 0
+        self.speedx = 0
+        self.speedy = 0
+
         self.hull = pygame.transform.scale(pygame.image.load("../Assets/Boat.png").convert_alpha(), ((1210*self.scale), (916*self.scale)))
         self.hullRect = self.hull.get_rect()
         self.tack = "port"
@@ -42,7 +54,9 @@ class Boat(pygame.sprite.Sprite):
         self.starboardSail = pygame.transform.scale(pygame.image.load("../Assets/starboardSail.png").convert_alpha(), ((1250*self.scale), (1250*self.scale)))
 
         self.sail = pygame.transform.scale(pygame.image.load("../Assets/portSail.png").convert_alpha(), ((1250*self.scale), (1250*self.scale)))
-        self.sailAngle = 0
+        self.sailAngle = 10
+        self.wind = localWind(self.hullRect.centerx, self.hullRect.centery)
+        self.angletoWind = (180 - self.angle % 180) - self.sailAngle - self.wind[1]
 
     def draw(self):
         tempHull = self.hull.copy()
@@ -58,10 +72,10 @@ class Boat(pygame.sprite.Sprite):
         self.hullRect.center = self.posx, self.posy
         screen.blit(tempHull, self.hullRect)
 
-    def rotate(self, angle):
-        self.angle = (self.angle + angle) % 360
+    def turn(self, angle):
+        self.angle = (self.angle + (angle * self.speed) / 1.5) % 360
 
-    def rotateSail(self, angle):
+    def trimSail(self, angle):
         if self.tack == "port":
             self.sailAngle = np.clip(((self.sailAngle + angle) % 360),10,100)
         elif self.tack == "starboard":
@@ -76,40 +90,44 @@ class Boat(pygame.sprite.Sprite):
             self.sail = self.portSail.copy()
             self.tack = "port"
             self.sailAngle = 100 - (self.sailAngle - 260)
+        elif self.angletoWind > 0 and self.tack == "port":
+            self.sail = self.portSail.copy()
+        elif self.angletoWind < 0 and self.tack == "port":
+            self.sail = self.starboardSail.copy()
+        elif self.angletoWind > 0 and self.tack == "starboard":
+            self.sail = self.starboardSail.copy()
+        elif self.angletoWind < 0 and self.tack == "starboard":
+            self.sail = self.portSail.copy()
 
-    def angularVelocity(self, keyspressed):
-        deceleration = 0.5
-        if keyspressed != 0:
-            angular_velocity
-            if self.angular_velocity > 0:
-                self.angular_velocity = max(0,self.angular_velocity - deceleration)
-            elif self.angular_velocity < 0:
-                self.angular_velocity = min(0, self.angular_velocity + deceleration)
+    def calcSpeed(self):
+        acceleration = self.acceleration()
+        print("Angle to Wind:", self.angletoWind)
+        print("Acceleration:", acceleration)
+        self.speed = np.clip(self.speed + acceleration, minSpeed*self.wind[0], maxSpeed(self.angletoWind)*self.wind[0])
+        print("Speed:", self.speed)
+        self.speedx = math.sin(math.radians(self.angle))*self.speed
+        print("Speed X:", self.speedx)
+        self.speedy = math.cos(math.radians(self.angle))*self.speed
+        print("Speed Y:", self.speedy)
 
-    # def direction(self):
-
-
-    def getVelocity(self):
-        wind = localWind(self.hullRect.centerx, self.hullRect.centery)
+    def acceleration(self):
+        self.wind = localWind(self.hullRect.centerx, self.hullRect.centery)
         if self.tack == "starboard":
-            angletoWind = self.angle - (100 - (self.sailAngle - 260)) - wind[1]
+            self.angletoWind = self.angle - (100 - (self.sailAngle - 260)) - self.wind[1]
         else:
-            angletoWind = (180 - self.angle % 180) - self.sailAngle - wind[1]
-        # print("Boat Angle:", self.angle, (180 - self.angle % 180))
-        # print("Sail Angle:", self.sailAngle)
-        # print("Tack:", self.tack)
-        print("Angle to wind:", angletoWind)
-        return speedfunction(angletoWind)*wind[0]
+            self.angletoWind = (180 - self.angle % 180) - self.sailAngle - self.wind[1]
+        return accelerationFunction(self.angletoWind)*self.wind[0]
 
     def update(self):
         self.move()
-        self.rotate((keys[pygame.K_d] - keys[pygame.K_a]) * 1)
+        self.turn((keys[pygame.K_d] - keys[pygame.K_a]))
         self.changeTack()
+        self.calcSpeed()
 
     def move(self):
-        velocity = self.getVelocity()
-        self.posx -= math.sin(math.radians(self.angle))*velocity
-        self.posy -= math.cos(math.radians(self.angle))*velocity
+        velocity = self.acceleration()
+        self.posx -= self.speedx
+        self.posy -= self.speedy
 
 keys = pygame.key.get_pressed()
 def tick():
@@ -135,9 +153,9 @@ while running:
         else:
             if event.type == pygame.MOUSEBUTTONDOWN:
                 if event.button == 5:
-                    player.rotateSail(1)
+                    player.trimSail(1)
                 elif event.button == 4:
-                    player.rotateSail(-1)
+                    player.trimSail(-1)
 
     keys = pygame.key.get_pressed()
     screen.fill((41, 74, 143))
